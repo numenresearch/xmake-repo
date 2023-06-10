a31a170d71ae0587813befbc011e9f215a03e5d6
zhouchao <2201441955@qq.com>
2023-06-10 10:16:52 +0800

    Update

--- packages/c/cpr/xmake.lua
+++ packages/c/cpr/xmake.lua
@@ -12,6 +12,7 @@ package("cpr")
     add_versions("1.9.4", "2fbb27716c010d8a28e52d5bc8f108e0d073ca3b3f5a48a2696b0231ea5196d5")
     add_versions("1.10.2", "044e98079032f7abf69c4c82f90ee2b4e4a7d2f28245498a5201ad6e8d0b1d08")
     add_versions("1.10.3", "d7f2574bd9dae8adb0ce6cf1afab119b509c297fffcb4204a1bb3e4e731074f2")
+    add_versions("1.10.4", "88462d059cd3df22c4d39ae04483ed50dfd2c808b3effddb65ac3b9aa60b542d")
 
     add_configs("ssl", {description = "Enable SSL.", default = false, type = "boolean"})
 
@@ -24,7 +25,7 @@ package("cpr")
     on_load(function (package)
         if package:config("ssl") then
             package:add("deps", "libcurl", {configs = {libssh2 = true, zlib = true}})
-            package:add("deps", "libssh2")
+            -- package:add("deps", "libssh2")
         else
             package:add("deps", "libcurl")
         end
@@ -43,7 +44,7 @@ package("cpr")
         end
         local packagedeps = {"libcurl"}
         if package:config("ssl") then
-            table.insert(packagedeps, "libssh2")
+            -- table.insert(packagedeps, "libssh2")
         end
         if package:is_plat("windows") then
             -- fix find_package issue on windows
